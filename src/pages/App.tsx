298b2a09aa64404e305f8d5353ad3b15ae379dd7
shendel <im_shendel@inbox.ru>
2023-05-25 18:51:04 +0300

    only pools

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -233,7 +233,7 @@ export default function App() {
               {isLoaded ? (
                 <Routes>
                   <Route path="/" element={<Landing />} />
-
+{/*
                   <Route path="tokens" element={<Tokens />}>
                     <Route path=":chainName" />
                   </Route>
@@ -247,23 +247,29 @@ export default function App() {
                     }
                   />
                   <Route path="create-proposal" element={<Navigate to="/vote/create-proposal" replace />} />
+                  */}
                   <Route path="send" element={<RedirectPathToSwapOnly />} />
                   <Route path="swap" element={<Swap />} />
 
+{/*
                   <Route path="pool/v2/find" element={<PoolFinder />} />
                   <Route path="pool/v2" element={<PoolV2 />} />
+                  */}
                   <Route path="pool" element={<Pool />} />
                   <Route path="pool/:tokenId" element={<PositionPage />} />
-
+{/*
                   <Route path="pools/v2/find" element={<PoolFinder />} />
                   <Route path="pools/v2" element={<PoolV2 />} />
+                  */}
                   <Route path="pools" element={<Pool />} />
                   <Route path="pools/:tokenId" element={<PositionPage />} />
 
+{/*
                   <Route path="add/v2" element={<RedirectDuplicateTokenIdsV2 />}>
                     <Route path=":currencyIdA" />
                     <Route path=":currencyIdA/:currencyIdB" />
                   </Route>
+                  */}
                   <Route path="add" element={<RedirectDuplicateTokenIds />}>
                     {/* this is workaround since react-router-dom v6 doesn't support optional parameters any more */}
                     <Route path=":currencyIdA" />
@@ -278,13 +284,13 @@ export default function App() {
                     <Route path=":currencyIdA/:currencyIdB/:feeAmount/:tokenId" />
                   </Route>
 
-                  <Route path="remove/v2/:currencyIdA/:currencyIdB" element={<RemoveLiquidity />} />
+                  {/*<Route path="remove/v2/:currencyIdA/:currencyIdB" element={<RemoveLiquidity />} />*/}
                   <Route path="remove/:tokenId" element={<RemoveLiquidityV3 />} />
-
+{/*
                   <Route path="migrate/v2" element={<MigrateV2 />} />
                   <Route path="migrate/v2/:address" element={<MigrateV2Pair />} />
-
-                  {!shouldDisableNFTRoutes && (
+*/}
+                  {!shouldDisableNFTRoutes || false && (
                     <>
                       <Route
                         path="/nfts"
